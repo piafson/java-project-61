bf9bdb02f8aa8d44fa4be73110c4e6803de644a9
piafson <pavorobyov@yandex.ru>
2023-09-18 09:36:09 +0800

    change Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,16 +6,15 @@ public class Even {
     private static final int[] RANGE_RAND = {4, 1000};
     private static final String GAME_QUES = "Answer 'yes' if the number is even"
             + ", otherwise answer 'no'.";
-    private static final String[][] GAME_INFO =
-            new String[Engine.COUNT_ROUNDS][2];
 
     public static void startEven() {
+        String[][] gameInfo = new String[Engine.COUNT_ROUNDS][2];
         for (int count = 0; count < Engine.COUNT_ROUNDS; count++) {
             int resInt = Utils.getRand(RANGE_RAND[0], RANGE_RAND[1]);
-            GAME_INFO[count][0] = Integer.toString(resInt);
-            GAME_INFO[count][1] =  isEven(resInt) ? "yes" : "no";
+            gameInfo[count][0] = Integer.toString(resInt);
+            gameInfo[count][1] =  isEven(resInt) ? "yes" : "no";
         }
-        Engine.run(GAME_QUES, GAME_INFO);
+        Engine.run(GAME_QUES, gameInfo);
     }
     public static boolean isEven(int num) {
         return (num % 2 == 0);
