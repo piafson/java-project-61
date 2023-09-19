fdeb7c4bf47e7767a5233a49a41d333072dee58b
piafson <pavorobyov@yandex.ru>
2023-09-19 11:07:25 +0800

    add scanner in Utils

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,20 +1,18 @@
 package hexlet.code;
-import java.util.Scanner;
+
 public class Engine {
     public static final int COUNT_ROUNDS = 3;
 
     public static void run(String question, String[][] result) {
         System.out.println('\n' + "Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
+        String name = Utils.getScan();
         System.out.println("Hello, " + name + "!");
         System.out.println(question);
         for (int i = 0; i < COUNT_ROUNDS; i++) {
             System.out.println("Question: " + result[i][0]);
             System.out.print("Your answer: ");
-            Scanner sc = new Scanner(System.in);
-            String answer = sc.nextLine();
+            String answer = Utils.getScan();
             if (result[i][1].equalsIgnoreCase(answer)) {
                 System.out.println("Correct!");
 
--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,9 +1,15 @@
 package hexlet.code;
 import java.util.Random;
+import java.util.Scanner;
 public class Utils {
     private static final Random RAND = new Random();
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int getRand(int min, int max) {
         return RAND.nextInt(min, max);
+    }
+
+    public static String getScan() {
+        return SCANNER.nextLine();
     }
 }
